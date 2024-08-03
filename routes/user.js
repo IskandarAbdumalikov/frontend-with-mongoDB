b97e0a9533b03b6a82d37fbbb33edae5e7fd58f9
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-08-03 16:00:16 +0500

    added

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { Users, validationUser } from "../schema/userScheme.js";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { auth } from "../middleware/auth.js";
+dotenv.config();
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const { limit = 5, skip = 1, gender } = req.query;
     const limitInt = parseInt(limit);
@@ -21,7 +26,7 @@ router.get("/", async (req, res) => {
         msg: "Users not found",
         variant: "warning",
         payload: null,
-        total:0
+        total: 0,
       });
     }
     const total = await Users.countDocuments(filter);
@@ -65,7 +70,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/sign-up", async (req, res) => {
   try {
     const { error, warning } = validationUser(req.body);
     if (error) {
@@ -82,12 +87,67 @@ router.post("/", async (req, res) => {
         payload: null,
       });
     }
+    const existUser = await Users.findOne({ username: req.body.username });
+    if (existUser) {
+      return res.status(400).json({
+        msg: "User already exists",
+        variant: "error",
+        payload: null,
+      });
+    }
 
+    req.body.password = await bcrypt.hash(req.body.password, 10);
     let user = await Users.create(req.body);
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
     res.status(201).json({
       msg: "User created",
       variant: "success",
-      payload: user,
+      payload: { user, token },
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Server error",
+      variant: "error",
+      payload: null,
+    });
+  }
+});
+
+router.post("/sign-in", async (req, res) => {
+  try {
+    const { password, username } = req.body;
+    const user = await Users.findOne({ username });
+    if (!user) {
+      return res.status(400).json({
+        msg: "Username or password error",
+        variant: "error",
+        payload: null,
+      });
+    }
+    bcrypt.compare(password, user.password, function (error, response) {
+      const token = jwt.sign(
+        { _id: user._id, role: "admin" },
+        process.env.SECRET_KEY
+      );
+      if (response) {
+        res.status(200).json({
+          msg: "Successfully login",
+          variant: "success",
+          payload: { user, token },
+        });
+      } else {
+        return res.status(400).json({
+          msg: "Username or password error",
+          variant: "error",
+          payload: null,
+        });
+      }
     });
   } catch (error) {
     res.status(500).json({
@@ -140,6 +200,17 @@ router.put("/:id", async (req, res) => {
       });
     }
 
+    const { username } = req.body;
+    const existingUser = await Users.findOne({ username });
+
+    if (existingUser && existingUser._id.toString() !== req.params.id) {
+      return res.status(400).json({
+        msg: "This username already exist",
+        variant: "error",
+        payload: null,
+      });
+    }
+
     let user = await Users.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
@@ -151,7 +222,7 @@ router.put("/:id", async (req, res) => {
       });
     }
     res.status(200).json({
-      msg: "User updated",
+      msg: "User successfully updated",
       variant: "success",
       payload: user,
     });
@@ -163,5 +234,6 @@ router.put("/:id", async (req, res) => {
     });
   }
 });
+
 
 export default router;
